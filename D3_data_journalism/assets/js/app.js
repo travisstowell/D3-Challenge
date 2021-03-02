ff7d4c6bf036ebc96c7a6fac2ecdcb50bc539fbf
Travis Stowell <71804152+travisstowell@users.noreply.github.com>
2021-03-02 15:53:08 -0600

    Pushing updates

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,102 @@
-// @TODO: YOUR CODE HERE!
+//Chart Set Up
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 50
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+//SVG wrapper,append an SVG group that will hold chart and set margins
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import data & format to numbers
+d3.csv("assets/data/data.csv").then(function(CensusData) {
+  CensusData.forEach(function(data) {
+    data.poverty = +data.poverty;
+    data.obesity = +data.obesity;
+    // console.log(data);
+  });
+
+  // Create Scales
+  const xScale = d3.scaleLinear()
+    .domain(d3.extent(CensusData, d => d.poverty))
+    .range([0, width])
+    .nice(); 
+
+  const yScale = d3.scaleLinear()
+    .domain([6,d3.max(CensusData, d => d.obesity)])
+    .range([height, 0])
+    .nice();
+  
+  // Create X & Y axis
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
+
+
+// Append X & Y axis to the chartGroup
+  chartGroup.append("g").attr("transform", `translate(0, ${height})`).call(xAxis);
+  chartGroup.append("g").call(yAxis);
+
+// Generate scatter plot 
+chartGroup.selectAll("circle")
+.data(CensusData)
+.enter()
+.append("circle")
+.attr("cx", d=>xScale(d.poverty))
+.attr("cy", d=>yScale(d.obesity))
+.attr("r", "10")
+.attr("stroke-width", "1")
+.classed("stateCircle", true)
+.attr("opacity", 0.75);
+
+// Add State abbr to each datapoint
+chartGroup.append("g")
+  .selectAll('text')
+  .data(CensusData)
+  .enter()
+  .append("text")
+  .text(d=>d.abbr)
+  .attr("x",d=>xScale(d.poverty))
+  .attr("y",d=>yScale(d.obesity))
+  .classed(".stateText", true)
+  .attr("font-family", "sans-serif")
+  .attr("text-anchor", "middle")
+  .attr("fill", "black")
+  .attr("font-size", "10px")
+  .style("font-weight", "bold")
+  .attr("alignment-baseline", "central");
+  
+  // Add X & Y axis titles
+  chartGroup.append("text")
+        .attr("transform", `translate(${width / 2}, ${height + margin.top + 13})`)
+        .attr("text-anchor", "middle")
+        .attr("font-size", "20px")
+        .attr("fill", "black")
+        .style("font-weight", "bold")
+        .text("Poverty %");
+
+        chartGroup.append("text")
+        .attr("y", 0 - ((margin.left / 2) + 2))
+        .attr("x", 0 - (height / 2))
+        .attr("text-anchor", "middle")
+        .attr("font-size", "20px")
+        .attr("fill", "black")
+        .style("font-weight", "bold")
+        .attr("transform", "rotate(-90)")
+        .text("Obesity (%)");
+}).catch(function(error) {
+  console.log(error);
+});
